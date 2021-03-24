b6cd8770bb35bdbab979d914d1fe03e5e59fe577
Sepideh <sepideh_shomali_5@yahoo.com>
2021-03-23 23:21:30 -0400

    installed react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+           {/* { <About />} */}
+           <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          ***about***Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -17,9 +45,55 @@ function App() {
         >
           Learn React
         </a>
+       
       </header>
     </div>
+          </Route>
+          <Route path="/users">
+            {/* <Users /> */}
+            <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         ***user*** Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+       
+      </header>
+    </div>
+          </Route>
+          <Route path="/">
+            {/* <Home /> */}
+            <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         ***home*** Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+       
+      </header>
+    </div>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+
+
